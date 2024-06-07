69a0f393ed4becedcb1fdf5314ac20747d3da9ce
mohammed <mohammed@mohammed.com>
2024-06-07 14:41:25 +0530

    channels 3 with hypercorn

--- server.py
+++ server.py
@@ -1,6 +1,22 @@
+'''
 from waitress import serve
 #from drflearn.wsgi import application
 from drflearn.asgi import application
 
 if __name__ == '__main__':
     serve(application, port=8000)
+
+'''
+
+import asyncio
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+
+from drflearn import asgi
+
+#asyncio.run(serve(asgi:application, Config()))
+result= asyncio.run(serve(asgi.application, Config(
+    
+    port=8000
+)))
+print(result)
